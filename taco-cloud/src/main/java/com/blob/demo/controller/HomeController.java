a8d01c4c948e225cfd48e49cb6dc67eba5c14231
Luis Cortes Coy <lfcortes12@gmail.com>
2019-06-26 17:43:43 -0500

    adds a basic view and controller

---
+++ taco-cloud/src/main/java/com/blob/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.blob.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+	@GetMapping(path = "/")
+	public String home() {
+		log.info("Executing home method ..");
+		return "home";
+
+	}
+
+}
